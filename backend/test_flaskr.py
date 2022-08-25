f6bb31ed49e5b5df898c94c83ef2817fee200ff6
unknown <DialloSouleymane1>
2022-08-25 16:41:15 +0000

    first commit

---
+++ backend/test_flaskr.py
@@ -0,0 +1,90 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from flaskr.models import setup_db, Question
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(
+            "postgres", "postgres", "localhost:5432", self.database_name
+        )
+        setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            "question": "What is Flask ?", 
+            "answer": "Micro Framework of Python", 
+            "category": 3,
+            "difficulty":2
+        }
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+    def test_get_paginated_questions(self):
+        res = self.client().get("/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+        self.assertTrue(len(data["categories"]))
+        self.assertTrue(len(data["current_category"]))
+
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get("/questions?page=1500", json={"page": 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+
+    def test_create_new_question(self):
+        res = self.client().post("/questions", json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["created"])
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+    
+    def test_delete_question(self):
+        res = self.client().delete("/questions/10")
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == 10).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["deleted"], 10)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+        self.assertEqual(question, None)
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
